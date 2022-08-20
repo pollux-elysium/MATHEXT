e62d6e48ff5211e3c64c4c45d0db45a141d38da3
Wayu Takemura <wayutakemura@gmail.com>
2022-08-20 22:10:13 +0700

    LinReg is now usable + more typedef

--- listelemop.py
+++ listelemop.py
@@ -1,14 +1,16 @@
-def mul(x: list, y: list):
+from .typedef import *
+
+def mul(x: list[number], y: list[number]) -> list[number]:
     return [i*j for i, j in zip(x, y)]
 
 
-def div(x: list, y: list):
+def div(x: list[number], y: list[number]) -> list[float]:
     return [i/j for i, j in zip(x, y)]
 
 
-def add(x: list, y: list):
+def add(x: list[number], y: list[number]) -> list[number]:
     return [i+j for i, j in zip(x, y)]
 
 
-def sub(x: list, y: list):
+def sub(x: list[number], y: list[number]) -> list[number]:
     return [i-j for i, j in zip(x, y)]
--- regression.py
+++ regression.py
@@ -1,18 +1,18 @@
-from typing import Union
+from typing import Literal, Union
 from .load import ldfe,ldf
 from .listelemop import *
-
-number = float|int
+from .typedef import *
 
 class Reg:
     x:list[number]
     fx:list[number]
 
-    def __init__(self,x:Union[list[number],"Reg"],fx:list[number]):
-        
+    def __init__(self,x:Union[list[number],"Reg"],fx:list[number]|Literal[False]=False) -> None:
+        if not fx:
+            fx=[]
         if isinstance(x,Reg):
             self.x=x.x
-            self,fx=x.fx
+            self.fx=x.fx
         elif len(x) == len(fx):
             self.x=x
             self.fx=fx
@@ -22,23 +22,25 @@ class Reg:
     @staticmethod
     def make(n:int=0,eval:bool=False):
         if eval:
-            x=[]
+            x:list[number]=[]
             print("Enter x:\n")
             ldfe(x,n)
-            fx=[]
+            fx:list[number]=[]
             print("Enter f(x):\n")
             ldfe(fx,n)
             return Reg(x,fx)
         else:
             x=[]
+            print("Enter x:\n")
             ldf(x,n)
             fx=[]
+            print("Enter f(x):\n")
             ldf(fx,n)
             return Reg(x,fx)
 
     @staticmethod
     def lot(n:list[tuple[number,number]]):
-        x,fx = [],[]
+        x:list[number] =[]; fx:list[number]= []
         for i in n:
             a,b=i
             x.append(a)
@@ -47,7 +49,8 @@ class Reg:
 
     def loadReg(self,reg:"Reg"):
         self.x=reg.x
-        self.y=reg.y
+        self.fx=reg.fx
+        return self
 
 class LinReg(Reg):
 
@@ -56,9 +59,18 @@ class LinReg(Reg):
         return LinReg(Reg.make(n,eval))
 
     @property
-    def c(self) ->float:
-        return (sum(self.fx)*sum(mul(self.x,self.x) - sum(self.x)*sum(mul(self.x,self.fx))))/(len(self.x)*sum(mul(self.x,self.x)-sum(self.x)**2))
+    def c(self):
+        return ((sum(self.fx)*sum(mul(self.x,self.x)) - sum(self.x)*sum(mul(self.x,self.fx)))
+            /(len(self.x)*sum(mul(self.x,self.x))-sum(self.x)**2))
 
     @property
-    def m(self) -> float:
-        return (len(self.x)*sum(mul(self.x,self.fx) - sum(self.x)*sum(self.fx)))/(len(self.x)*sum(mul(self.x,self.x)-sum(self.x)**2))
+    def m(self):
+        return ((
+            len(self.x)*sum(mul(self.x,self.fx)) - sum(self.x)*sum(self.fx))
+            /(len(self.x)*sum(mul(self.x,self.x))-sum(self.x)**2))
+
+    def f(self,x:number):
+        return self.m*x+self.c
+
+    def xf(self,f:number):
+        return (f-self.c)/self.m
