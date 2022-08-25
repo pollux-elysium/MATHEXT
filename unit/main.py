d2dd493b7a1e4793898cb0f69abc7648867468b7
Wayu Takemura <wayutakemura@gmail.com>
2022-08-26 00:02:26 +0700

    Unit Untested WIP

--- unit/main.py
+++ unit/main.py
@@ -1,10 +1,3 @@
 from ..typedef import *
-from .DimAndUnit import CompoundUnit
+from .DimAndUnit import CompoundUnit,CompoundUnitResolvables,resolveCompoundUnit
 
-class Value:
-    unit: CompoundUnit
-    value:number
-
-    def __init__(self,value:number,unit:CompoundUnit):
-        self.value=value
-        self.unit=unit
