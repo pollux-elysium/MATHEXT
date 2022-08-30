ab1af553c4d403995e37e76af4b6d088b9d1a3a7
Wayu Takemura <wayutakemura@gmail.com>
2022-08-30 15:06:01 +0700

    Added eigenvalues

---
+++ unit/constants.py
@@ -0,0 +1,11 @@
+from .DimAndUnit import Value,CompoundUnit
+from .unitdef import *
+from math import pi
+
+
+R = Value(8.314427,joule/mol/kelvin)
+g = Value(9.80665,meter/sec*sec)
+mu0 = Value(4*pi*1e-7,meter*meter/sec*sec)
+R_atm = Value(0.008206,liter*atm/mol/kelvin)
+Na = 6.02214129e23
+h = Value(6.626070040e-34,joule*sec)
