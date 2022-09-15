25c5a3a84577b9fc88d5c9c5ab396e69950f79b3
Wayu Takemura <wayutakemura@gmail.com>
2022-09-15 15:25:27 +0700

    added perm and comb

--- combinatorics.py
+++ combinatorics.py
@@ -1,3 +1,9 @@
 from math import factorial
 def binomial(n:int,k:int):
     return factorial(n)/(factorial(k)*factorial(n-k))
+
+def perm(n:int,k:int):
+    return factorial(n)/factorial(n-k)
+
+def comb(n:int,k:int):
+    return perm(n,k)/factorial(k)
