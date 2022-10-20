9725a89f3c51b968172f4e6f9d9b518fcffe2190
Wayu Takemura <wayutakemura@gmail.com>
2022-10-20 10:36:42 +0700

    Fixed some value

--- unit/constants.py
+++ unit/constants.py
@@ -6,6 +6,6 @@ from math import pi
 R = Value(8.314427,joule/mol/kelvin)
 g = Value(9.80665,meter/sec*sec)
 mu0 = Value(4*pi*1e-7,meter*meter/sec*sec)
-R_atm = Value(0.008206,liter*atm/mol/kelvin)
+R_atm = Value(0.08206,liter*atm/mol/kelvin)
 Na = 6.02214129e23
 h = Value(6.626070040e-34,joule*sec)
