58db4a80a1e1da7a2b2f5401ef8c4bab0112545b
Wayu Takemura <wayutakemura@gmail.com>
2023-04-26 15:25:03 +0700

    fixed g unit

--- unit/constants.py
+++ unit/constants.py
@@ -4,7 +4,7 @@ from math import pi
 
 
 R = Value(8.31446261815324,joule/mol/kelvin)
-g = Value(9.80665,meter/sec*sec)
+g = Value(9.80665,meter/sec/sec)
 mu0 = Value(4*pi*1e-7,meter*meter/sec*sec)
 R_atm = Value(0.08206,liter*atm/mol/kelvin)
 Na = 6.02214129e23
