e3566c3e3ded3357cfb35de288c4d3324b0c41e6
Wayu Takemura <wayutakemura@gmail.com>
2023-11-15 14:27:46 +0700

    New function imports

--- __init__.py
+++ __init__.py
@@ -8,7 +8,7 @@ import numpy as np
 from itertools import accumulate as cum, combinations as com, permutations as per
 from statistics import mean, median as med, geometric_mean as gmean, harmonic_mean as hmean, stdev, pstdev, variance as var, pvariance as pvar
 from math import degrees as deg, radians as rad, sin, cos, tan, asin, acos, atan, hypot, atan2, sqrt, pi, isclose,log
-from .load import ldi,ldie,lda,ldae,ldc,ldf,ldfe,lda3d
+from .load import ldi,ldie,lda,ldae,ldc,ldf,ldfe,lda3d,ltsvf,ltsvi
 from .stats import *
 from .listelemop import *
 from .poly import *
@@ -16,7 +16,7 @@ from .degRad import dsin,dcos,dtan
 from .vec import drec,drec3d,dpol,dpol3d,rec3d,pol3d,v3d
 from .matrix import Mat,AugMat
 from .geometry import line,plane
-from .regression import LinReg,QuadReg,ExpReg,LogReg,Reg,bestFit
+from .regression import LinReg,QuadReg,CubReg,ExpReg,LogReg,Reg,bestFit,PolyReg
 from .combinatorics import binomial,perm,comb
 from .distribution import DiscreteDistribution,UniformDistribution,BinomialDistribution,GeometricDistribution,HypergeometricDistribution,JointDiscreteDistribution
 from .contDistribution import ContinuousDistribution,ContinuousUniformDistribution,NormalDistribution,pZ,DefaultND,x,oo,Zp,binomialZ
