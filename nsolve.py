b4bbf2258f268bb0cbb2d1eab314e367af9e94ef
Wayu Takemura <wayutakemura@gmail.com>
2024-02-14 22:17:30 +0700

    added debug option

--- nsolve.py
+++ nsolve.py
@@ -1,11 +1,20 @@
 from .typedef import number
 from typing import Callable
+from sys import stderr
 
-def invFuncSolve(func:Callable[[number],number],y:number,start:number=1,err:number=1e-6) -> number:
+def invFuncSolve(func:Callable[[number],number],y:number,start:number=1,err:number=1e-6,debug:bool = False) -> number:
     """
     Solve for the inverse of a function using Newton's method.
     """
     x = start
     while abs(func(x)-y)>err:
+        if debug:
+            print("x:", x,file=stderr)
+            print("f(x):", func(x),file=stderr)
+            print("(f(x)-y):", func(x)-y,file=stderr)
+            print("f(x+err):", func(x+err),file=stderr)
+            print("(f(x+err)-f(x)):", func(x+err)-func(x),file=stderr)
+            print("(f(x+err)-f(x))/err:", (func(x+err)-func(x))/err,file=stderr)
+            print("Delta x:", -(func(x)-y)/((func(x+err)-func(x))/err),file=stderr)
         x = x - (func(x)-y)/((func(x+err)-func(x))/err)
     return x
