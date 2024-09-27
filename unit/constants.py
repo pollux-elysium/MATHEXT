ef11284f9b272b766891eb4bc0b72d4b39c95794
Wayu Takemura <wayutakemura@gmail.com>
2024-09-27 23:16:08 +0700

    Add more constants

--- unit/constants.py
+++ unit/constants.py
@@ -6,6 +6,8 @@ from math import pi
 R = Value(8.31446261815324,joule/mol/kelvin)
 g = Value(9.80665,meter/sec/sec)
 mu0 = Value(4*pi*1e-7,meter*meter/sec*sec)
+eps0 = Value(8.854187817e-12,coulomb/volt/meter)
 R_atm = Value(0.08206,liter*atm/mol/kelvin)
 Na = 6.02214129e23
 h = Value(6.626070040e-34,joule*sec)
+G = Value(6.67408e-11,meter*meter*meter/kilogram/sec/sec)
